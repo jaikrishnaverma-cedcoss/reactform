6314b2c24e96212ff0066d742c5a054dea41a532
jaikrishnaverma <jaikrishnaverma>
2022-09-06 15:22:23 +0530

    task1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './BaseUI.css';
+import './App.css';
+import Task1 from './tasks/Task1';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Q1.Create a text box that accepts only numbers and display the same using buttons</h3>
+       <Task1 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tasks/Task1.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+function Task1() {
+    const [input,setInput]=useState("")
+    const [para,setPara]=useState("")
+    const inpuHandler=(event)=>{
+        if(!isNaN(event.target.value))
+        setInput(event.target.value)
+    }
+    const showsdata=()=>
+    {
+        setPara(input)
+    }
+    return ( 
+        <>
+        <div className="row">
+            <textarea style={{padding:"10px"}} onChange={inpuHandler} value={input} id="" cols="30" rows="10"></textarea>
+            <button className="btn btn-danger" onClick={showsdata}>Check</button>
+        </div>
+        <div className="row">
+            <p>{para}</p>
+        </div>
+        </>
+     );
+}
+
+export default Task1;

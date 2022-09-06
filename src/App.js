4f79b5cc7628e71e2266d38981b5e78d02dba392
jaikrishnaverma <jaikrishnaverma>
2022-09-06 16:10:15 +0530

    Task3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './BaseUI.css';
 import './App.css';
 import Task1 from './tasks/Task1';
 import Task2 from './tasks/Task2';
+import Task3 from './tasks/Task3';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,8 @@ function App() {
        <Task1 />
        <h3>Q2.Create a Text area and convert the text into uppercase using button</h3>
        <Task2 />
+       <h3>Q3.Create a Dropdown list that has entries from 1 to 7, onchange of this dropdown the respective day must be printe</h3>
+       <Task3 />
       </header>
     </div>
   );
--- src/tasks/Task3.jsx
+++ src/tasks/Task3.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 function Task3() {
+    let day=useRef();
+    const getDate=(event)=>{
+        day.current.innerText= event.target.value
+    }
     return (
          <>
-         
+         <div className="col">
+            <select name="" id="" onChange={getDate}>
+                <option value="Sun">1</option>
+                <option value="Mon">2</option>
+                <option value="Tue">3</option>
+                <option value="Wed">4</option>
+                <option value="Thu">5</option>
+                <option value="Fri">6</option>
+                <option value="Sat">7</option>
+            </select>
+            <h2 ref={day} style={{color:"pink"}}></h2>
+         </div>
        </> 
     );
 }
